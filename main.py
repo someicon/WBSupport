a892b8fc9bffa4e117567a74c96d6ed8a34ec9e1
Сергей Ельмеев <s.m.elmeev@metropolis.russia>
2024-07-05 11:36:57 +0300

    12

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-from aiogram import Dispatcher
+from aiogram import Dispatcher, Bot, types
+
+dp = Dispatcher()
+bot = Bot(token="7358435330:AAEsvCM4sM1Qjluwu3AZJvmc_q44cbyndg4")
+
+
+# Обработчик событий для бота. @dp.message() отлавливает все входящие сообщения
+@dp.message()
+async def echo(message: types.Message) -> None:
+    await message.answer(text="Привет, это бот технический поддержки, чем я могу помочь ?")
+
+
+
+
+async def main():
+    await dp.start_polling(bot)     # main() Начинает опрашивать сервера через диспетчер и передает обновления боту
+
+
+if __name__
