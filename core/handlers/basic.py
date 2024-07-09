ee518912e68129bce826f55f00116fafed963131
Сергей Ельмеев <s.m.elmeev@metropolis.russia>
2024-07-09 17:50:29 +0300

    FSM Context

--- core/handlers/basic.py
+++ core/handlers/basic.py
@@ -1,11 +1,18 @@
-from aiogram import Bot, F, Router
+from aiogram import F, Router
 from aiogram.filters import Command, CommandStart
 from aiogram.types import Message, CallbackQuery
+from aiogram.fsm.state import StatesGroup, State
+from aiogram.fsm.context import FSMContext
 
 import core.keyboards.all_keyboards as kb
 
 
 router = Router()
+
+
+class Reg(StatesGroup):
+    name = State()
+    number = State()
 
 
 # Обработчик событий для бота. @dp.message() отлавливает все входящие сообщения
@@ -15,18 +22,42 @@ async def echo(message: Message) -> None:
                          reply_markup=kb.main)
 
 
-@router.message(F.text == "/help")
-async def get_help(message: Message) -> None:
-    await message.reply("Выберете пункт меню: ")
-
-
-@router.message(F.text == "Позвонить")
-async def get_phone(message: Message) -> None:
-    await message.answer("Телефоны тех поддрежки : 1801, 1802, 1803, 1804")
-
-
-# Обработчик для команды /start
 @router.callback_query(F.data == "catalog")
 async def get_catalog(callback: CallbackQuery):
-    await callback.answer("")
-    await callback.message.edit_text("Вы нажали кнопку Каталог",reply_markup= await kb.inline_items())
+    await callback.message.edit_text("Каталог", reply_markup=kb.catalog)
+
+
+@router.callback_query(F.data == "headphone")
+async def get_main_menu(callback: CallbackQuery):
+    await callback.message.edit_text("Наушники", reply_markup=kb.headphone)
+
+
+@router.callback_query(F.data == "catalog_menu")
+async def get_main_menu(callback: CallbackQuery):
+    await callback.message.edit_text("Каталог", reply_markup=kb.catalog)
+
+
+@router.callback_query(F.data == "main_menu")
+async def get_main_menu(callback: CallbackQuery):
+    await callback.message.edit_text("Главное меню", reply_markup=kb.main)
+
+
+@router.message(Command("reg"))
+async def reg_first(message: Message, state: FSMContext):
+    await state.set_state(Reg.name) # Установка состояния Регистрация.name
+    await message.answer("Введите ваше имя")
+
+
+@router.message(Reg.name) # Фильтр отлавливает состояние Регистрация.name
+async def reg_second(message: Message, state: FSMContext):
+    await state.update_data(name=message.text) # Сохранение введенной информации (Имя пользователя)
+    await state.set_state(Reg.number)   # Смена состояния на Регистрация.number
+    await message.answer("Введите ваш номер телефона")
+
+
+@router.message(Reg.number)
+async def reg_third(message: Message, state: FSMContext):
+    await state.update_data(number=message.text)
+    data = await state.get_data() # data достает всю введенную информацию
+    await message.answer(f"Регистрация завершена\nИмя: {data["name"]}\nНомер: {data["number"]}")
+    await state.clear()
--- core/keyboards/all_keyboards.py
+++ core/keyboards/all_keyboards.py
@@ -8,6 +8,20 @@ main = InlineKeyboardMarkup(inline_keyboard=[
      InlineKeyboardButton(text="Контакты", callback_data="contacts")],
 ])
 
+catalog = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text="Вешалки", callback_data="hanger")],
+    [InlineKeyboardButton(text="Ковры", callback_data="carpet")],
+    [InlineKeyboardButton(text="Наушники", callback_data="headphone")],
+    [InlineKeyboardButton(text="На главную", callback_data="main_menu")]])
+
+
+headphone = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text="AirPods 3", callback_data="hanger")],
+    [InlineKeyboardButton(text="AirPods Pro 2", callback_data="carpet")],
+    [InlineKeyboardButton(text="AirPods Pro", callback_data="headphone")],
+    [InlineKeyboardButton(text="Назад", callback_data="catalog_menu")],
+    [InlineKeyboardButton(text="На главную", callback_data="main_menu")]])
+
 
 settings = InlineKeyboardMarkup(inline_keyboard=[
     [InlineKeyboardButton(text="YouTube", url="https://www.youtube.com/watch?v=6i52YJqNziI"),
@@ -19,12 +33,13 @@ help_kb = ReplyKeyboardMarkup(keyboard=[
     resize_keyboard=True,
     input_field_placeholder="Выберете пункт меню")
 
-items = ["Вешалки", "Ковры", "Наушники"]
 
-
-async def inline_items():
-    keyboard = InlineKeyboardBuilder()
-    for item in items:
-        keyboard.add(InlineKeyboardButton(text=item,
-                                          url="https://www.youtube.com/watch?v=6i52YJqNziI"))
-    return keyboard.adjust(2).as_markup()
+# items = ["Вешалки", "Ковры", "Наушники"]
+#
+#
+# async def inline_items():
+#     keyboard = InlineKeyboardBuilder()
+#     for item in items:
+#         keyboard.add(InlineKeyboardButton(text=item,
+#                                           url="https://www.youtube.com/watch?v=6i52YJqNziI"))
+#     return keyboard.adjust(2).as_markup()
